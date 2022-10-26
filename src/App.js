943ab300127f64cdf38cd920a81caa0d69211983
GuyenTu8765 <tunguyen.080701@gmail.com>
2022-10-27 01:59:31 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Exercise1.Question1 /> */}
+      {/* <Exercise1.Question2p1 /> */}
+      {/* <Exercise1.Question2p2/> */}
+
+      {/* <Exercise2.Question1a /> */}
+      {/* <Exercise2.Question1b /> */}
+      {/* chuaa xong */}
+      {/* <Exercise2.Question1c /> */}
+
+      {/* <Exercise2.Question2/> */}
+      {/* chua xong */}
+      {/* <Exercise2.Question3 /> */}
+      <Exercise2.Question4 />
+      {/* <Exercise2.Question1f/> */}
+
+      {/* <Exercise4.Question1 fullName="Nguyen Van Tu" age="21" />
+    <Exercise4.Question1 fullName="Nguyen Van " age="21">
+      choi khi con tre
+    </Exercise4.Question1>
+    <Exercise4.Question1 fullName="Nguyen Van bay" age="21">
+      boc dau to lai
+    </Exercise4.Question1> */}
     </div>
   );
 }
