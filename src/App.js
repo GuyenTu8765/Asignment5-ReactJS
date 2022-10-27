a77a62f0c6a0045af1c7ad159ca1b3e46caa26ec
GuyenTu8765 <tunguyen.080701@gmail.com>
2022-10-28 01:56:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Exercise1 from "./Exercise/Exercise1";
+import Exercise2 from "./Exercise/Exercise2";
 
 function App() {
   return (
     <div>
       {/* <Exercise1.Question1 /> */}
-      {/* <Exercise1.Question2p1 /> */}
-      {/* <Exercise1.Question2p2/> */}
+      {/* <Exercise1.Question2 /> */}
+      {/* <Exercise1.Question3a />
+      <Exercise1.Question3b /> */}
 
-      {/* <Exercise2.Question1a /> */}
-      {/* <Exercise2.Question1b /> */}
+      <Exercise2.Question1aFun />
+      <Exercise2.Question1aClass />
       {/* chuaa xong */}
       {/* <Exercise2.Question1c /> */}
 
       {/* <Exercise2.Question2/> */}
       {/* chua xong */}
       {/* <Exercise2.Question3 /> */}
-      <Exercise2.Question4 />
+      {/* <Exercise2.Question4 /> */}
       {/* <Exercise2.Question1f/> */}
 
       {/* <Exercise4.Question1 fullName="Nguyen Van Tu" age="21" />
--- src/Exercise/Exercise1.js
+++ src/Exercise/Exercise1.js
@@ -1,126 +1,214 @@
 import React, { Component } from "react";
 import "./Exercise1.css";
-import "antd/dist/antd.css";
-import {  Table } from 'antd';
+// import "antd/dist/antd.css";
+// import { Table } from "antd";
 
-function Question1() {
-  return (
-    <React.Fragment>
-      <p>Some text.</p>
-      <h1>A heading</h1>
-      <p>More text.</p>
-      <h1>Another heading</h1>
-      <p>Even more text.</p>
-      </React.Fragment>
-  );
-}
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'Trần Đức Anh',
-    date: '03/08/1993',
-    sex: 'Nam',
-  },
-  {
-    key: '2',
-    name: 'Kieu Thi Thu Trang',
-    date: '04/09/1991',
-    sex: 'Nu',
-  },
-  {
-    key: '3',
-    name: 'Vuong Thi Le Na',
-    date: '06/10/1991',
-    sex: 'Nu',
-  },
-];
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Date',
-    dataIndex: 'date',
-    key: 'date',
-  },
-  {
-    title: 'Sex',
-    dataIndex: 'sex',
-    key: 'sex',
-  },
-
-];
-
-const Question2p1 = () => {
-  return <Table dataSource={dataSource} columns={columns} />
-}
-
-const TableHeader = () => {
-  return (
-    <thead>
-      <tr>
-        <th className="bordersName">Name</th>
-        <th className="borders1">Date</th>
-        <th className="borders1">Sex</th>
-      </tr>
-    </thead>
-  );
+const BoilingVerdict = (props) => {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
 };
+//  Calculator
+class Question1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "" };
+  }
 
-const TableBody = (props) => {
-  const rows = props.characterData.map((item, index) => {
-    return (
-      <tr key={index}>
-        <td className="bordersName">{item.name}</td>
-        <td className="borders1">{item.date}</td>
-        <td className="borders1">{item.sex}</td>
-      </tr>
-    );
-  });
-  return <tbody>{rows}</tbody>;
-};
-
-class Table1 extends Component {
+  handleChange(event) {
+    this.setState({ temperature: event.target.value });
+  }
   render() {
-    const { characterData } = this.props;
+    const temperature = this.state.temperature;
     return (
-      <table className="bordersName">
-        <TableHeader />
-        <TableBody characterData={characterData} />
-      </table>
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
     );
   }
 }
 
-class Question2p2 extends Component {
+const InputName = (props) => {
+  const handleChangeInput = (event) => {
+    props.changeInputName(event.target.value);
+  };
+
+  return (
+    <div>
+      <label>
+        Name:
+        <input
+          type={"text"}
+          value={props.name}
+          onChange={handleChangeInput}
+        ></input>
+      </label>
+    </div>
+  );
+};
+
+class Question2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+
+  receiveInputChanging = (newName) => {
+    this.setState({
+      name: newName,
+    });
+  };
+
   render() {
-    const characters = [
-      {
-        name: "Trần Đức Anh",
-        date: "03/08/1993",
-        sex: "Nam",
-      },
-      {
-        name: "Kieu Thi Thu Trang",
-        date: "04/09/1991",
-        sex: "Nu",
-      },
-      {
-        name: "Vuong Thi Le Na",
-        date: "06/10/1991",
-        sex: "Nu",
-      },
-    ];
     return (
-      <div className="container">
-        <Table1 characterData={characters} />
+      <div>
+        <InputName
+          name={this.state.name}
+          changeInputName={this.receiveInputChanging}
+        />
+        <div>Hello {this.state.name} </div>
       </div>
     );
   }
 }
 
-export default { Question1, Question2p1, Question2p2 };
+class TemperatureInput extends React.Component {
+  render() {
+    return (
+      <fieldset>
+        <legend>Enter temperature in {this.props.typeTemperature}: </legend>
+        <input
+          value={this.props.temperature}
+          onChange={(event) =>
+            this.props.onTemperatureChange(event.target.value)
+          }
+        />
+      </fieldset>
+    );
+  }
+}
+
+class Question3a extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: "",
+    };
+  }
+
+  toCelsius = (fahrenheit) => {
+    return ((fahrenheit - 32) * 5) / 9;
+  };
+
+  toFahrenheit = (celsius) => {
+    return (celsius * 9) / 5 + 32;
+  };
+
+  handleCelsiusChange = (celsius) => {
+    this.setState({
+      temperature: celsius,
+    });
+  };
+
+  handleFahrenheitChange = (fahrenheit) => {
+    this.setState({
+      temperature: this.toCelsius(fahrenheit),
+    });
+  };
+
+  render() {
+    const celsius = this.state.temperature === "" ? "" : this.state.temperature;
+    const fahrenheit =
+      this.state.temperature === ""
+        ? ""
+        : this.toFahrenheit(this.state.temperature);
+    return (
+      <div>
+        <TemperatureInput
+          temperature={celsius}
+          typeTemperature="Celsius"
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          temperature={fahrenheit}
+          typeTemperature="Fahrenheit"
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+      </div>
+    );
+  }
+}
+
+const USDtoVND = (props) => {
+  const convert = (usd) => {
+    return usd * 24845;
+  };
+  return (
+    <>
+      <span>USD </span>
+      <input
+        onChange={(event) => {
+          const usd = event.target.value;
+          const vnd = convert(usd);
+          props.onHandleChange({
+            usd,
+            vnd,
+          });
+        }}
+        value={props.value}
+      />
+    </>
+  );
+};
+
+const VNDtoUSD = (props) => {
+  const convert = (vnd) => {
+    return vnd / 24845;
+  };
+  return (
+    <>
+      <span>VND </span>
+      <input
+        onChange={(event) => {
+          const vnd = event.target.value;
+          const usd = convert(vnd);
+          props.onHandleChange({
+            usd,
+            vnd,
+          });
+        }}
+        value={props.value}
+      />
+    </>
+  );
+};
+
+class Question3b extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usd: 0,
+      vnd: 0,
+    };
+  }
+
+  handleChange = (data) => {
+    this.setState(data);
+  };
+
+  render() {
+    return (
+      <>
+        <USDtoVND onHandleChange={this.handleChange} value={this.state.usd} />
+        <VNDtoUSD onHandleChange={this.handleChange} value={this.state.vnd} />
+      </>
+    );
+  }
+}
+export default { Question1, Question2, Question3a, Question3b };
